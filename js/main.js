acaa82019a274008e246687a28b3895d0aadcd02
Alexandra Sorokina <alexandrasorokina@Alexandras-iMac.local>
2019-08-20 16:23:06 -0700

    remake of the project without react

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// jQuery Smooth scroll
+// $('.navbar a').on('click', function(e){
+//     if(this.hash !== '') {
+//         e.preventDefault();
+//         // Hash value into a variable
+//         const hash = this.hash;
+//         // target body and call animate method
+//         //animate method is taking an object with animate property
+//         // offset method 
+//         //second parameter is speed, which is 800 mlseconds
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top},
+//             800
+//         );
+//     }
+// });
+
+
+
+// Option 3 to implement smooth scroll
+const scroll = new SmoothScroll('.navbar a[href*="#"]');
+
+//Slide for projects
+
+// $(function() {   
+//     $('.slide').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 200,
+//         prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
+//           // next arrow
+//           nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
+//           centerMode: true,
+//           // Side padding when in center mode. (px or %)
+//           centerPadding: '50px',
+//           // CSS3 easing
+//           cssEase: 'ease',
+//           // Current slide indicator dots
+//           dots: true,
+//           // Class for slide indicator dots container
+//           dotsClass: 'slick-dots',
+//             // Enables desktop dragging
+//             draggable: true,
+//             // animate() fallback easing
+//             easing: 'linear',
+
+
+
+
+//      });
+//    });
